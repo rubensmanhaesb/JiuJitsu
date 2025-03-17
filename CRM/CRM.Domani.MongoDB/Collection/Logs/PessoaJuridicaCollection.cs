50277ece4d3926c5c674907e216714edd4edd0a5
rubensmanhaesb <115162453+rubensmanhaesb@users.noreply.github.com>
2025-03-16 22:29:56 -0300

    fix: Arquivos ignorados do commit anterior incorretamente

---
+++ CRM/CRM.Domani.MongoDB/Collection/Logs/PessoaJuridicaCollection.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace CRM.Domain.MongoDB.Collection.Logs
+{
+    public class PessoaJuridicaCollection
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        [BsonElement("RazaoSocial")]
+        public string? RazaoSocial { get; set; }
+        [BsonElement("NomeFantasia")]
+        public string? NomeFantasia { get; set; }
+        [BsonElement("Cnpj")]
+        public string? Cnpj { get; set; }
+        [BsonElement("Email")]
+        public string? Email { get; set; }
+    }
+}
